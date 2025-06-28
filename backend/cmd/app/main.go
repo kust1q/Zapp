b7b2d990fab49a975888714a94a921d3e6147eec
kust1q <dimamishin2133@mail.ru>
2025-06-28 18:13:23 +0300

    init config file

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -7,15 +7,25 @@ import (
 	"github.com/kust1q/Zapp/backend/internal/repository"
 	"github.com/kust1q/Zapp/backend/internal/servers"
 	"github.com/kust1q/Zapp/backend/internal/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handler := http.NewHandler(services)
 
 	srv := new(servers.Server)
-	if err := srv.Run("8080", handler.InitRouters()); err != nil {
-		log.Fatalf("error occured while running http server %s", err.Error())
+	if err := srv.Run(viper.GetString("port"), handler.InitRouters()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
