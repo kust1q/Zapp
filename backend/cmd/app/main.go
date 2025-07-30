096f968572232d47ddeb1647cd94092550a4beb1
kust1q <dimamishin2133@mail.ru>
2025-07-30 23:36:20 +0300

    up main

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,53 +1,46 @@
 package main
 
 import (
-	"os"
-
-	"github.com/joho/godotenv"
+	"github.com/kust1q/Zapp/backend/internal/config"
 	"github.com/kust1q/Zapp/backend/internal/delivery/http"
 	"github.com/kust1q/Zapp/backend/internal/repository"
+	"github.com/kust1q/Zapp/backend/internal/repository/minio"
+	"github.com/kust1q/Zapp/backend/internal/repository/postgres"
 	"github.com/kust1q/Zapp/backend/internal/servers"
 	"github.com/kust1q/Zapp/backend/internal/service"
-	"github.com/kust1q/Zapp/backend/pkg/repository/postgres"
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	//logrus.SetFormatter(new(logrus.JSONFormatter))
-	if err := initConfig(); err != nil {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := config.InitConfig(); err != nil {
 		logrus.Fatalf("error initializing config: %s", err.Error())
 	}
 
-	if err := godotenv.Load(); err != nil {
-		logrus.Fatalf("error .env loading: %s", err.Error())
-	}
+	cfg := config.Get()
 
-	db, err := postgres.NewPostgresDB(postgres.Config{
-		Host:     viper.GetString("db.host"),
-		Port:     viper.GetString("db.port"),
-		Username: os.Getenv("POSTGRES_USER"),
-		Password: os.Getenv("POSTGRES_PASSWORD"),
-		DBName:   os.Getenv("POSTGRES_DB"),
-		SSLMode:  viper.GetString("db.sslmode"),
-	})
+	db, err := postgres.NewPostgresDB(cfg.Postgres)
 	if err != nil {
 		logrus.Fatalf("failed to init db: %s", err.Error())
 	}
 
-	repos := repository.NewRepository(db)
-	services := service.NewService(repos)
-	handler := http.NewHandler(services)
+	mc, err := minio.NewMinioClient(cfg.Minio)
+	if err != nil {
+		logrus.Fatalf("failed to init minio client: %s", err.Error())
+	}
+	err = minio.CreateBucket(mc, cfg.Minio)
+	if err != nil {
+		logrus.Fatalf("failed to create minio bucket: %s", err.Error())
+	}
+
+	handler := http.NewHandler(
+		service.NewAuthService(repository.NewAuthStorage(db, mc)),
+	)
 
 	srv := new(servers.Server)
-	if err := srv.Run(viper.GetString("port"), handler.InitRouters()); err != nil {
+	if err := srv.Run(cfg.App.Port, handler.InitRouters()); err != nil {
 		logrus.Fatalf("error occured while running http server: %s", err.Error())
 	}
-}
-
-func initConfig() error {
-	viper.AddConfigPath("configs")
-	viper.SetConfigName("config")
-	return viper.ReadInConfig()
 }
